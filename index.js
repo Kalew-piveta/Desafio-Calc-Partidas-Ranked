19d975c71aebdae702cdfa86cff5cbf8eff3731a
Kalew-piveta <kalew1310@gmail.com>
2024-01-11 16:29:21 -0300

    Código desafio completo

---
+++ index.js
@@ -0,0 +1,57 @@
+/*
+ # Calculadora de partidas Rankeadas
+**O Que deve ser utilizado**
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+- Funções
+
+## Objetivo:
+
+Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
+depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+
+Se vitórias for menor do que 10 = Ferro
+Se vitórias for entre 11 e 20 = Bronze
+Se vitórias for entre 21 e 50 = Prata
+Se vitórias for entre 51 e 80 = Ouro
+Se vitórias for entre 81 e 90 = Diamante
+Se vitórias for entre 91 e 100= Lendário
+Se vitórias for maior ou igual a 101 = Imortal
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+"O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
+*/
+// Função para determinar o nível com base no número de vitórias
+
+function determinarNivel(vitorias) {
+    if (vitorias < 10) {
+      return 'Ferro';
+    } else if (vitorias <= 20) {
+      return 'Bronze';
+    } else if (vitorias <= 50) {
+      return 'Prata';
+    } else if (vitorias <= 80) {
+      return 'Ouro';
+    } else if (vitorias <= 90) {
+      return 'Diamante';
+    } else if (vitorias <= 100) {
+      return 'Lendário';
+    } else {
+      return 'Imortal';
+    }
+  }
+  
+const prompt = require('prompt-sync')()
+let vitorias = prompt("Digite a quantidade de Vitórias: ")
+let derrotas = prompt("Digite a quantidade de Derrotas: ")
+vitorias = parseInt(vitorias)
+derrotas = parseInt(derrotas)
+saldoVitorias = vitorias - derrotas
+let nivel = determinarNivel(vitorias)
+  
+console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`);
